85ccd5baaef04e88bde07f0390c4009ef674dd9f
Leandro Pessoa Santos <leandro.psantos@spread.com.br>
2018-04-24 16:30:17 -0300

    Subindo TesteJava

---
+++ src/test/java/foradacaixa/ClienteElementMap.java
@@ -0,0 +1,15 @@
+package foradacaixa;
+
+import org.openqa.selenium.WebElement;
+
+public class ClienteElementMap {
+	protected WebElement cpf_cnpj;
+	protected WebElement fisica;
+	protected WebElement juridica;
+	protected WebElement nome_razaosocial;
+	protected WebElement email;
+	protected WebElement data_nascimento;
+	protected WebElement sexo;
+	protected WebElement estado_civil;
+	protected WebElement avancar;
+}
---
+++ src/test/java/foradacaixa/ClienteSteps.java
@@ -0,0 +1,49 @@
+package foradacaixa;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ClienteSteps {
+	
+	@Given("efetuei login no sistema")
+	public void efetuarLogin() {
+
+	}
+
+	@And("acessei o menu Clientes >> Inserir")
+	public void acessarMenuClientesIserir() {
+
+	}
+
+	@When("na tela Dados de Identificacao informo os dados de Pessoa Fisica")
+	public void informarDadosIdentificacaoPF() {
+
+	}
+
+	@And("na tela Dados de Identificacao clico em Avancar")
+	public void clicarAvancar() {
+
+	}
+
+	@And("na tela Enderecos informo os enderecos")
+	public void informarEnderecos() {
+
+	}
+
+	@And("na tela Enderecos clico em Salvar")
+	public void clicarSalvar() {
+
+	}
+
+	@Then("na tela Enderecos sera exibida mensagem de sucesso")
+	public void verificarMensagemSucesso() {
+
+	}
+
+	@And("efetuarei logout do sistema")
+	public void efetuarLogout() {
+
+	}
+
+}
